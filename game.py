aa3961a056a5c71cbb750ff598df96e9c87c6ee0
Lucas Venícius Alves Santos <lucas.alves.santos19@gmail.com>
2023-10-08 21:29:21 -0300

    Adicionada classe GeniusComputerPlayer que faz com que este player use o algoritmo minimax para determinar a melhor jogada possivel a cada ciclo fazendo com que esse player nunca perca, ele ganha ou empata apenas.

--- game.py
+++ game.py
@@ -1,4 +1,4 @@
-from player import HumanPlayer, RandomComputerPlayer
+from player import HumanPlayer, RandomComputerPlayer, GeniusComputerPlayer
 import time
 
 class TicTacToc:
@@ -89,15 +89,28 @@ def play(game, x_player, o_player, print_game=True):
             letter = "O" if letter == "X" else "X"
             
         # tiny break
-        time.sleep(1)
+        if print_game:
+            time.sleep(1)
         
     if print_game:
         print("It\'s a tie!")
             
 
 if __name__ == "__main__":
-    x_player = HumanPlayer("X")
-    o_player = RandomComputerPlayer("O")
-    t = TicTacToc()
-    play(t, x_player, o_player, print_game=True)
+    x_wins = 0
+    o_wins = 0
+    ties = 0
+    for _ in range(1000):
+        x_player = RandomComputerPlayer("X")
+        o_player = GeniusComputerPlayer("O")
+        t = TicTacToc()
+        result = play(t, x_player, o_player, print_game=False)
+        if result == "X":
+            x_wins += 1
+        elif result == "O":
+            o_wins += 1
+        else:
+            ties += 1
+        
+    print(f"Afer  1000 iterations, we see {x_wins} X wins, {o_wins} O wins, {ties} ties.")
     
--- player.py
+++ player.py
@@ -41,3 +41,69 @@ class HumanPlayer(Player):
                 print("Invalid square. Try again.")
                 
         return val
+    
+    
+class GeniusComputerPlayer(Player):
+    def __init__(self, letter):
+        super().__init__(letter)
+        
+    def get_move(self, game):
+        if len(game.available_moves()) == 9:
+            square = random.choice(game.available_moves()) #randomly choose one
+        else:
+            # get the square based off the minimax algorithm
+            square = self.minimax(game, self.letter)["position"]
+        return square
+    
+    def minimax(self, state, player):
+        max_player = self.letter # yourself
+        other_player = "O" if player == "X" else "X" # the other player
+        
+        # first, we want to check if the previous move is a winner
+        # this is our base case
+        if state.current_winner == other_player:
+            # we should return position and score because we want to keep track of the score
+            # for minimax to work
+            return {
+                "position": None,
+                "score": 1 * (state.number_empty_squares() + 1) if other_player == max_player 
+                else -1 * (state.number_empty_squares() + 1)
+            }
+
+        elif not state.empty_squares(): # no empty squares
+            return {
+                "position":None,
+                "score": 0
+            }
+            
+        if player == max_player:
+            best = {
+                "position": None,
+                "score": -math.inf #each score should maximize (be larger)
+            }
+        else:
+            best = {
+                "position": None,
+                "score": math.inf #each score should minimize
+            }
+        for possible_move in state.available_moves():
+            #step 1: make a move, try that spot
+            state.make_move(possible_move, player)
+            
+            #step 2: recurse using minimax to simulate a game after making that move
+            sim_score = self.minimax(state, other_player) #now, we alternate players
+            
+            #step 3: undo the move
+            state.board[possible_move] = " "
+            state.current_winner = None
+            sim_score["position"] = possible_move
+            
+            #step 4: update the dictionaries if necessary
+            if player == max_player:
+                if sim_score["score"] > best["score"]:
+                    best = sim_score #replace best
+            else:
+                if sim_score["score"] < best["score"]:
+                    best = sim_score #replace best
+                        
+        return best
